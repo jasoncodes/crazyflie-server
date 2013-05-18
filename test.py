e3cfee3565f7a90201fa6010073e455c6a1c7b74
Jason Weathered <jason@jasoncodes.com>
2013-05-18 13:42:51 +1000

    Read point input in JSON format

--- test.py
+++ test.py
@@ -13,30 +13,18 @@ import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cfclient.utils.logconfigreader import LogVariable, LogConfig
 
-class Main:
-    quit = False
-    roll = 0
-    pitch = 0
-    yawrate = 0
-    thrust = 0
+import json
 
+class Main:
     def __init__(self):
         self.crazyflie = Crazyflie()
         cflib.crtp.init_drivers()
-
-        interfaces = cflib.crtp.scan_interfaces()
-        print interfaces
-        for interface in interfaces:
-            print "Found %s" % (interface[0])
 
         self.crazyflie.open_link("radio://0/10/250K")
 
         self.crazyflie.connectSetupFinished.add_callback(self.connectSetupFinished)
 
     def connectSetupFinished(self, linkURI):
-        pulse_thread = Thread(target=self.pulse_loop)
-        pulse_thread.start()
-
         input_thread = Thread(target=self.input_loop)
         input_thread.start()
 
@@ -62,64 +50,28 @@ class Main:
         else:
             print "stabilizer not found in log TOC"
 
-    def is_number(self, s):
-        try:
-            int(s)
-            return True
-        except ValueError:
-            return False
-
     def input_loop(self):
+        self.quit = False
         while not self.quit:
             try:
-                str = raw_input()
+                input = json.loads(raw_input())
+            except ValueError:
+                print json.dumps({'debug': 'invalid JSON received'})
+                input = {}
             except EOFError:
                 self.quit = True
                 break
 
-            if str == 'z':
-                self.quit = True
-                break
-
-            if str == 'q':
-                if self.thrust <= 10000:
-                    self.thrust = 10001
-                else:
-                    self.thrust += 1000
-            elif str == 'a':
-                self.thrust -= 1000
-            elif str == 'w':
-                if self.thrust <= 10000:
-                    self.thrust = 20000
-                else:
-                    self.thrust += 5000
-            elif str == 's':
-                self.thrust -= 5000
-            elif str == 'x':
-                self.pitch = 0
-                self.roll = 0
-                if self.thrust > 35000:
-                    self.thrust = 35000
-                    time.sleep(3)
-                self.thrust = 0
-            elif str == 'i': # forward
-                self.pitch += 1
-            elif str == 'k': # back
-                self.pitch -= 1
-            elif str == 'j': # left
-                self.roll -= 1
-            elif str == 'l': # right
-                self.roll += 1
-            else:
-                if self.is_number(str):
-                    self.thrust = 10000 + int(50000 * int(str) / 9.0)
-
-            print "thrust = %d, pitch = %5.02f, roll = %5.02f" % (self.thrust, self.pitch, self.roll)
-
-    def pulse_loop(self):
-        while not self.quit:
-            self.crazyflie.commander.send_setpoint(self.roll, self.pitch, self.yawrate, self.thrust)
-            time.sleep(0.1)
+            try:
+                point = input['point']
+                self.crazyflie.commander.send_setpoint(
+                    point['roll'],
+                    point['pitch'],
+                    point['yaw'],
+                    point['thrust']
+                )
+            except:
+                print json.dumps({'debug': 'error processing point data'})
 
         self.crazyflie.commander.send_setpoint(0, 0, 0, 0)
         time.sleep(0.1)
